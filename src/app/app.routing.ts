bf177629a032c1cd3dded0b1884c877c3e54f886
olmedo <olmedo,bdp@gmail.com>
2020-06-29 11:51:17 -0500

    pagina de error, routing

---
+++ src/app/app.routing.ts
@@ -0,0 +1,28 @@
+//importar modulos de router de angular
+import {ModuleWithProviders} from '@angular/core';
+import {Routes ,RouterModule} from '@angular/router';
+
+//importar componentyes q necesitan pagina en concreto
+import { HomeComponent} from './components/home/home.component'
+import { BlogComponent} from './components/blog/blog.component'
+import { FormularioComponent} from './components/formulario/formulario.component'
+import { PaginaComponent} from './components/pagina/pagina.component'
+import { PeliculasComponent} from './components/peliculas/peliculas.component'
+import { ErrorComponent } from './components/error/error.component';
+
+//Aarrays de rutas
+
+const appRoutes: Routes = [
+    {path: '', component:HomeComponent},
+    {path: 'home', component: HomeComponent},
+    {path: 'blog', component: BlogComponent},
+    {path: 'formulario', component: FormularioComponent},
+    {path: 'peliculas', component: PeliculasComponent},
+    {path: 'pagina-pruebas', component: PaginaComponent},
+    {path: '**', component: ErrorComponent}
+];
+
+
+//exportar modulo de rutas
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
