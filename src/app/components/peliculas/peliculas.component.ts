1d237a2ab3a123440f6a68271f1ca076eb1ff518
olmedo <olmedo,bdp@gmail.com>
2020-06-29 14:24:55 -0500

    109 directiva ng style ngClass

--- src/app/components/peliculas/peliculas.component.ts
+++ src/app/components/peliculas/peliculas.component.ts
@@ -14,10 +14,10 @@ export class PeliculasComponent implements OnInit, DoCheck, OnDestroy {
 
     this.titulo = "componente pelicula tirulo"
     this.peliculas = [
-      {title: "Spiderman 4", image: 'https://i0.pngocean.com/files/203/497/288/spider-man-comic-book-clip-art-spiderman.jpg'},
-      {title: "Los vengadores ", image: 'https://i0.pngocean.com/files/203/497/288/spider-man-comic-book-clip-art-spiderman.jpg'},
-      {title: "supermamn ", image: 'https://as.com/meristation/imagenes/2020/05/28/noticias/1590658176_046742_1590658235_noticia_normal_recorte1.jpg'},
-      {title: "tittacnic", image: 'https://as.com/meristation/imagenes/2020/05/28/noticias/1590658176_046742_1590658235_noticia_normal_recorte1.jpg'},
+      {year: 2018, title: "Spiderman 4", image: 'https://i0.pngocean.com/files/203/497/288/spider-man-comic-book-clip-art-spiderman.jpg'},
+      {year: 2019, title: "Los vengadores ", image: 'https://i0.pngocean.com/files/203/497/288/spider-man-comic-book-clip-art-spiderman.jpg'},
+      {year: 2017, title: "supermamn ", image: 'https://as.com/meristation/imagenes/2020/05/28/noticias/1590658176_046742_1590658235_noticia_normal_recorte1.jpg'},
+      {year: 2015, title: "tittacnic", image: 'https://as.com/meristation/imagenes/2020/05/28/noticias/1590658176_046742_1590658235_noticia_normal_recorte1.jpg'},
     ]
    }
 
